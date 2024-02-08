25530d8e5a3c8188a39341031937cf5ee5a4aea7
Andrew Pham <69804999+andrewp8@users.noreply.github.com>
2024-02-08 18:25:01 +0000

    fisnihed route Director

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -2,4 +2,23 @@ class DirectorsController < ApplicationController
   def index
     render({:template => "directors_templates/list"})
   end
+
+  def show
+    the_id = params.fetch("the_id")
+
+    """ 
+      - where({:id => the_id})
+        + .where() is one of **Active Record** skill
+        + the hash, {}, inside the parenthesis is called **hash literal**
+        + The return value from calling .where() is called an **Relation**
+          + A relation: a collection of records, doesn't matter if it is 1 or many records, [[],...], so we access the target record the same way we access the element of an array
+    """
+    matching_records = Director.where({:id => the_id})
+    
+    @the_director = matching_records.at(0)
+
+    @matching_movies = Movie.where({:director_id => @the_director.id})
+
+    render({:template => "directors_templates/details"})
+  end
 end
